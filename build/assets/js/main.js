e6c290363732e8d2da5573c73770727e9d0a2e20
dVitold <tigronixx@gmail.com>
2019-09-15 00:05:56 +0300

    card, counter, tooltip, catalog. catalog calculations

--- build/assets/js/main.js
+++ build/assets/js/main.js
@@ -11,327 +11,113 @@ var APP = {
 };
 
 (function () {
-	// sliders
-	var personCardSlider = function personCardSlider() {
-		var $sliders = $('.js-person-card-slider');
+	// specific
+	var cardCalc = function cardCalc() {
+		var cards = document.querySelectorAll('.js-card');
+		var cardObj = {
+			'cardArr': [],
+			'calcPrice': function calcPrice(index) {
+				var quantity = this.cardArr[index].quantity;
+				var price = this.cardArr[index].price;
 
-		$sliders.slick({
-			slidesToShow: 5,
-			infinite: false,
-			responsive: [{
-				breakpoint: 768,
-				settings: {
-					slidesToShow: 3
+				var summ = quantity * price;
+
+				return this.cardArr[index].summ = summ;
+			},
+			'quantityMinus': function quantityMinus(index) {
+				var quantity = this.cardArr[index].quantity;
+				quantity--;
+
+				if (quantity <= 0) {
+					quantity = 1;
 				}
-			}]
-		});
-	};
 
-	var advantagesSlider = function advantagesSlider() {
-		var $sliders = $('.js-advantages-slider');
-		var options = {
-			slidesToShow: 3,
-			infinite: false
-		};
+				return this.cardArr[index].quantity = quantity;
+			},
+			'quantityPlus': function quantityPlus(index) {
+				var quantity = this.cardArr[index].quantity;
+				quantity++;
 
-		$(window).on('load resize orientationchange', function () {
-			$sliders.each(function () {
-				var $slider = $(this);
-				var isDesktop = $(window).width() > 768;
-				var isSliderActive = $slider.hasClass('slick-initialized');
+				return this.cardArr[index].quantity = quantity;
+			},
+			'calcTotalQuantity': function calcTotalQuantity(index) {
+				var totalQuantityArr = [];
 
-				if (isDesktop) {
-					if (!isSliderActive) {
-						$slider.slick(options);
-					}
-				} else {
-					if (isSliderActive) {
-						$slider.slick('unslick');
-					}
-				}
-			});
-		});
-	};
+				this.cardArr.forEach(function (elem) {
+					totalQuantityArr.push(parseInt(elem.quantity));
+				});
 
-	var popularServices = function popularServices() {
-		var $sliders = $('.js-services-slider');
-		var options = {
-			slidesToShow: 1,
-			infinite: false,
-			mobileFirst: true,
-			dots: true
-		};
+				var result = totalQuantityArr.reduce(function (sum, current) {
+					return sum + current;
+				}, 0);
 
-		$(window).on('load resize orientationchange', function () {
-			$sliders.each(function () {
-				var $slider = $(this);
-				var isDesktop = $(window).width() > 768;
-				var isSliderActive = $slider.hasClass('slick-initialized');
-
-				if (isDesktop) {
-					if (isSliderActive) {
-						$slider.slick('unslick');
-					}
-				} else {
-					if (!isSliderActive) {
-						$slider.slick(options);
-					}
-				}
-			});
-		});
-	};
-
-	// others
-	var selectChange = function selectChange() {
-		var selectWraps = document.querySelectorAll('.js-select-wrap');
-
-		selectWraps.forEach(function (selectWrap) {
-			var select = selectWrap.querySelector('.js-select');
-
-			select.addEventListener('change', function () {
-				select.classList.add('changed');
-			});
-		});
-	};
-
-	var phoneMask = function phoneMask() {
-		var $phoneInputs = $('.form-elem--phone');
-
-		$phoneInputs.mask('+7 (000)-000-00-00'), {
-			placeholder: '123'
-		};
-	};
-
-	var checkboxLink = function checkboxLink() {
-		var checkboxLinks = document.querySelectorAll('.js-checkbox-link');
-
-		checkboxLinks.forEach(function (checkboxLink) {
-			var input = checkboxLink.querySelector('.checkbox__input');
-			var href = checkboxLink.getAttribute('data-href');
-
-			input.addEventListener('change', function () {
-				var isChecked = this.checked;
-
-				if (isChecked) {
-					location.replace(href);
-				}
-			});
-		});
-	};
-
-	var menu = function menu() {
-		var body = document.querySelector('body');
-		var aside = document.querySelector('.js-aside');
-		var btn = document.querySelector('.js-btn-menu');
-		var imgOpen = btn.querySelector('.js-menu-img-open');
-		var imgClose = btn.querySelector('.js-menu-img-close');
-
-		var open = function open(btn) {
-			btn.classList.add('active');
-			aside.classList.add('active');
-			body.classList.add('overflow');
-		};
-
-		var close = function close(btn) {
-			btn.classList.remove('active');
-			aside.classList.remove('active');
-			body.classList.remove('overflow');
-		};
-
-		btn.addEventListener('click', function () {
-			var isActive = this.classList.contains('active');
-
-			if (isActive) {
-				close(this);
-			} else {
-				open(this);
+				this.totalQuantity = result;
 			}
-		});
-	};
+		};
 
-	var avtoSelect = function avtoSelect() {
-		var totalText = {};
-		var currentStep = 1;
-		var $form = $('.js-form-help');
-		var form = document.querySelector('.js-form-help');
-		var selectBrand = document.getElementById('brand');
-		var selectModel = document.getElementById('model');
-		var selectMalfunction = document.getElementById('malfunction');
-		var infoSelectors = document.querySelectorAll('.js-total-text');
-		var steps = document.querySelectorAll('.js-steps');
-		var steps1 = document.querySelectorAll('.js-steps-1');
-		var steps2 = document.querySelectorAll('.js-steps-2');
-		var steps3 = document.querySelectorAll('.js-steps-3');
-		var btnNextStep = document.querySelector('.js-btn-next-step');
-		var btnSubmit = document.querySelector('.js-btn-submit');
-		var carsWrap = document.querySelector('.js-form-cars');
+		cards.forEach(function (card, index) {
+			var basketCounter = document.querySelector('.js-basket-counter');
+			var counter = card.querySelector('.js-counter');
+			var counterMinusBtn = counter.querySelector('.js-counter-prev');
+			var counterPlusBtn = counter.querySelector('.js-counter-next');
+			var counterNumber = counter.querySelector('.js-counter-number');
+			var inputFirst = card.querySelector('.js-card-input');
+			var inputs = card.querySelectorAll('.js-card-input');
+			var price = card.querySelector('.js-card-price');
 
-		$form.each(function () {
-			// utility functions
-			var formReset = function formReset() {
-				$form[0].reset();
+			var insertText = function insertText() {
+				counterNumber.innerText = cardObj.cardArr[index].quantity;
+				price.innerText = cardObj.cardArr[index].summ;
+				basketCounter.innerText = cardObj.totalQuantity;
 			};
 
-			var disableNextBtn = function disableNextBtn() {
-				btnNextStep.classList.add('disabled');
+			// data for obj
+			var quantity = counterNumber.innerText;
+			var defaultPrice = inputFirst.getAttribute('data-default-price');
+
+			var itemObj = {
+				quantity: quantity,
+				price: defaultPrice
 			};
 
-			var permitNextBtn = function permitNextBtn() {
-				btnNextStep.classList.remove('disabled');
-			};
-
-			var closeSteps = function closeSteps() {
-				var steps = document.querySelectorAll('.js-steps');
-
-				steps.forEach(function (step) {
-					step.classList.add('hide');
-					step.classList.remove('active');
-				});
-			};
-
-			var goToStep = function goToStep(steps) {
-				closeSteps();
-
-				steps.forEach(function (step) {
-					step.classList.add('active');
-					step.classList.remove('hide');
-				});
-			};
-
-			var refreshOrderStatus = function refreshOrderStatus() {
-				infoSelectors.forEach(function (infoSelector) {
-					var textNodes = infoSelector.querySelectorAll('span');
-
-					textNodes.forEach(function (textNode) {
-						textNode.remove();
-					});
-
-					infoSelector.insertAdjacentHTML('afterbegin', '<span>' + totalText.total + '</span>');
-				});
-			};
-
-			var resetModelSelect = function resetModelSelect(modelOptions) {
-				selectModel.selectedIndex = 0;
-
-				var selectModelWrap = selectModel.closest('.form-elem__wrap');
-				selectModelWrap.classList.remove('checked');
-			};
-
-			var closeOptions = function closeOptions(modelOptions) {
-				modelOptions.forEach(function (modelOption) {
-					modelOption.classList.add('hide');
-					modelOption.classList.remove('matched');
-				});
-			};
-
-			var openOptions = function openOptions(modelOption) {
-				modelOption.classList.remove('hide');
-				modelOption.classList.add('matched');
-			};
+			// data created
+			cardObj.cardArr.push(itemObj);
 
 			// listeners
-			formReset();
+			counterMinusBtn.addEventListener('click', function () {
+				cardObj.quantityMinus(index);
+				cardObj.calcPrice(index);
+				cardObj.calcTotalQuantity();
 
-			selectBrand.addEventListener('change', function () {
-				var brandValue = selectBrand.options[selectBrand.selectedIndex].value;
-				var brandText = selectBrand.options[selectBrand.selectedIndex].text;
-				var modelOptions = selectModel.querySelectorAll('option');
-				var selectWrap = selectBrand.closest('.form-elem__wrap');
+				insertText();
+			});
 
-				totalText.brand = brandText;
+			counterPlusBtn.addEventListener('click', function () {
+				cardObj.quantityPlus(index);
+				cardObj.calcPrice(index);
+				cardObj.calcTotalQuantity();
 
-				selectWrap.classList.add('checked');
+				insertText();
+			});
 
-				resetModelSelect(modelOptions);
-				closeOptions(modelOptions);
+			inputs.forEach(function (input) {
+				input.addEventListener('change', function () {
+					var defaultPrice = input.getAttribute('data-default-price');
 
-				modelOptions.forEach(function (modelOption) {
-					var modelGroup = modelOption.getAttribute('data-group');
-					var isMatched = modelGroup === brandValue;
+					cardObj.cardArr[index].price = defaultPrice;
+					cardObj.calcPrice(index);
+					cardObj.calcTotalQuantity();
 
-					if (isMatched) {
-						openOptions(modelOption);
-					}
+					insertText();
 				});
-
-				disableNextBtn();
-			});
-
-			selectModel.addEventListener('change', function () {
-				var modelText = selectModel.options[selectModel.selectedIndex].text;
-				var selectWrap = selectModel.closest('.form-elem__wrap');
-
-				selectWrap.classList.add('checked');
-				totalText.model = modelText;
-
-				totalText.total = totalText.brand + ' ' + totalText.model;
-
-				refreshOrderStatus();
-				permitNextBtn();
-			});
-
-			selectMalfunction.addEventListener('change', function () {
-				var selectWrap = selectMalfunction.closest('.form-elem__wrap');
-				var malfunctionText = selectMalfunction.options[selectMalfunction.selectedIndex].text;
-
-				selectWrap.classList.add('checked');
-
-				totalText.malfunction = malfunctionText;
-
-				totalText.total = totalText.brand + ' ' + totalText.model + ', ' + totalText.malfunction;
-
-				refreshOrderStatus();
-			});
-
-			btnNextStep.addEventListener('click', function () {
-				closeSteps();
-
-				if (currentStep >= 2) {
-					this.classList.add('hide');
-					console.log(this);
-
-					currentStep = 2;
-				}
-
-				var nextStep = currentStep + 1;
-				var nextStepNodesStroke = '.js-steps-' + nextStep;
-				var nextStepNodes = document.querySelectorAll('.js-steps-' + nextStep);
-
-				currentStep++;
-
-				// car animation
-				form.classList.remove('step-1');
-				form.classList.remove('step-2');
-				form.classList.add('step-' + nextStep);
-
-				goToStep(nextStepNodes);
 			});
 		});
 	};
 
-	var headerScroll = function headerScroll() {
-		var $header = $('.js-header');
-
-		$(window).on('scroll', function () {
-			if ($(this).scrollTop() > 50) {
-				$header.addClass('scrolled');
-			} else {
-				$header.removeClass('scrolled');
-			}
-		});
+	var onPageRdy = function onPageRdy() {
+		cardCalc();
 	};
 
-	// sliders
-	personCardSlider();
-	advantagesSlider();
-	popularServices();
-
-	// others
-	selectChange();
-	phoneMask();
-	checkboxLink();
-	menu();
-	avtoSelect();
-	headerScroll();
+	onPageRdy();
 })();
 //# sourceMappingURL=main.js.map
--- src/js/scripts/scripts.js
+++ src/js/scripts/scripts.js
@@ -1,324 +1,110 @@
 (function () {
-	// sliders
-	const personCardSlider = function () {
-		const $sliders = $('.js-person-card-slider');
+	// specific
+	const cardCalc = function () {
+		const cards = document.querySelectorAll('.js-card');
+		const cardObj = {
+			'cardArr': [],
+			'calcPrice': function (index) {
+				const quantity = this.cardArr[index].quantity;
+				const price = this.cardArr[index].price;
 
-		$sliders.slick({
-			slidesToShow: 5,
-			infinite: false,
-			responsive: [{
-				breakpoint: 768,
-				settings: {
-					slidesToShow: 3
+				const summ = quantity * price;
+
+				return this.cardArr[index].summ = summ;
+			},
+			'quantityMinus': function (index) {
+				let quantity = this.cardArr[index].quantity;
+				quantity--;
+
+				if(quantity <= 0) {
+					quantity = 1;
 				}
-			}, ]
-		});
-	};
 
-	const advantagesSlider = function () {
-		const $sliders = $('.js-advantages-slider');
-		const options = {
-			slidesToShow: 3,
-			infinite: false
-		};
+				return this.cardArr[index].quantity = quantity;
+			},
+			'quantityPlus': function (index) {
+				let quantity = this.cardArr[index].quantity;
+				quantity++;
 
-		$(window).on('load resize orientationchange', function () {
-			$sliders.each(function () {
-				const $slider = $(this);
-				const isDesktop = $(window).width() > 768;
-				const isSliderActive = $slider.hasClass('slick-initialized');
+				return this.cardArr[index].quantity = quantity;
+			},
+			'calcTotalQuantity': function (index) {
+				const totalQuantityArr = [];
 
-				if(isDesktop) {
-					if(!isSliderActive) {
-						$slider.slick(options);
-					}
-				} else {
-					if(isSliderActive) {
-						$slider.slick('unslick');
-					}
-				}
-			});
-		});
-	};
-
-	const popularServices = function () {
-		const $sliders = $('.js-services-slider');
-		const options = {
-			slidesToShow: 1,
-			infinite: false,
-			mobileFirst: true,
-			dots: true
-		};
-
-		$(window).on('load resize orientationchange', function () {
-			$sliders.each(function () {
-				const $slider = $(this);
-				const isDesktop = $(window).width() > 768;
-				const isSliderActive = $slider.hasClass('slick-initialized');
-
-				if(isDesktop) {
-					if(isSliderActive) {
-						$slider.slick('unslick');
-					}
-				} else {
-					if(!isSliderActive) {
-						$slider.slick(options);
-					}
-				}
-			});
-		});
-	};
-
-	// others
-	const selectChange = function () {
-		const selectWraps = document.querySelectorAll('.js-select-wrap');
-
-		selectWraps.forEach(function (selectWrap) {
-			const select = selectWrap.querySelector('.js-select');
-
-			select.addEventListener('change', function () {
-				select.classList.add('changed');
-			});
-		});
-	};
-
-	const phoneMask = function () {
-		const $phoneInputs = $('.form-elem--phone');
-
-		$phoneInputs.mask('+7 (000)-000-00-00'), {
-			placeholder: '123'
-		};
-	};
-
-	const checkboxLink = function () {
-		const checkboxLinks = document.querySelectorAll('.js-checkbox-link');
-
-		checkboxLinks.forEach(function (checkboxLink) {
-			const input = checkboxLink.querySelector('.checkbox__input');
-			const href = checkboxLink.getAttribute('data-href');
-
-			input.addEventListener('change', function () {
-				const isChecked = this.checked;
-
-				if(isChecked) {
-					location.replace(href);
-				}
-			});
-		});
-	};
-
-	const menu = function () {
-		const body = document.querySelector('body');
-		const aside = document.querySelector('.js-aside');
-		const btn = document.querySelector('.js-btn-menu');
-		const imgOpen = btn.querySelector('.js-menu-img-open');
-		const imgClose = btn.querySelector('.js-menu-img-close');
-
-		const open = function (btn) {
-			btn.classList.add('active');
-			aside.classList.add('active');
-			body.classList.add('overflow');
-		};
-
-		const close = function (btn) {
-			btn.classList.remove('active');
-			aside.classList.remove('active');
-			body.classList.remove('overflow');
-		};
-
-		btn.addEventListener('click', function () {
-			const isActive = this.classList.contains('active');
-
-			if(isActive) {
-				close(this);
-			} else {
-				open(this);
-			}
-		});
-	};
-
-	const avtoSelect = function () {
-		const totalText = {};
-		let currentStep = 1;
-		const $form = $('.js-form-help');
-		const form = document.querySelector('.js-form-help');
-		const selectBrand = document.getElementById('brand');
-		const selectModel = document.getElementById('model');
-		const selectMalfunction = document.getElementById('malfunction');
-		const infoSelectors = document.querySelectorAll('.js-total-text');
-		const steps = document.querySelectorAll('.js-steps');
-		const steps1 = document.querySelectorAll('.js-steps-1');
-		const steps2 = document.querySelectorAll('.js-steps-2');
-		const steps3 = document.querySelectorAll('.js-steps-3');
-		const btnNextStep = document.querySelector('.js-btn-next-step');
-		const btnSubmit = document.querySelector('.js-btn-submit');
-		const carsWrap = document.querySelector('.js-form-cars');
-
-		$form.each(function () {
-			// utility functions
-			const formReset = function () {
-				$form[0].reset();
-			};
-
-			const disableNextBtn = function () {
-				btnNextStep.classList.add('disabled');
-			};
-
-			const permitNextBtn = function () {
-				btnNextStep.classList.remove('disabled');
-			};
-
-			const closeSteps = function () {
-				const steps = document.querySelectorAll('.js-steps');
-
-				steps.forEach(function (step) {
-					step.classList.add('hide');
-					step.classList.remove('active');
+				this.cardArr.forEach(function (elem) {
+					totalQuantityArr.push(parseInt(elem.quantity));
 				});
+
+				const result = totalQuantityArr.reduce(function (sum, current) {
+					return sum + current;
+				}, 0);
+
+				this.totalQuantity = result;
+			},
+		};
+
+		cards.forEach(function (card, index) {
+			const basketCounter = document.querySelector('.js-basket-counter');
+			const counter = card.querySelector('.js-counter');
+			const counterMinusBtn = counter.querySelector('.js-counter-prev');
+			const counterPlusBtn = counter.querySelector('.js-counter-next');
+			const counterNumber = counter.querySelector('.js-counter-number');
+			const inputFirst = card.querySelector('.js-card-input');
+			const inputs = card.querySelectorAll('.js-card-input');
+			const price = card.querySelector('.js-card-price');
+
+			const insertText = function () {
+				counterNumber.innerText = cardObj.cardArr[index].quantity;
+				price.innerText = cardObj.cardArr[index].summ;
+				basketCounter.innerText = cardObj.totalQuantity;
 			};
 
-			const goToStep = function (steps) {
-				closeSteps();
+			// data for obj
+			const quantity = counterNumber.innerText;
+			const defaultPrice = inputFirst.getAttribute('data-default-price');
 
-				steps.forEach(function (step) {
-					step.classList.add('active');
-					step.classList.remove('hide');
-				});
+			const itemObj = {
+				quantity: quantity,
+				price: defaultPrice
 			};
 
-			const refreshOrderStatus = function () {
-				infoSelectors.forEach(function (infoSelector) {
-					const textNodes = infoSelector.querySelectorAll('span');
-
-					textNodes.forEach(function (textNode) {
-						textNode.remove();
-					});
-
-					infoSelector.insertAdjacentHTML('afterbegin', `<span>${totalText.total}</span>`);
-				});
-			};
-
-			const resetModelSelect = function (modelOptions) {
-				selectModel.selectedIndex = 0;
-
-				const selectModelWrap = selectModel.closest('.form-elem__wrap');
-				selectModelWrap.classList.remove('checked');
-			};
-
-			const closeOptions = function (modelOptions) {
-				modelOptions.forEach(function (modelOption) {
-					modelOption.classList.add('hide');
-					modelOption.classList.remove('matched');
-				});
-			};
-
-			const openOptions = function (modelOption) {
-				modelOption.classList.remove('hide');
-				modelOption.classList.add('matched');
-			};
+			// data created
+			cardObj.cardArr.push(itemObj);
 
 			// listeners
-			formReset();
+			counterMinusBtn.addEventListener('click', function () {
+				cardObj.quantityMinus(index);
+				cardObj.calcPrice(index);
+				cardObj.calcTotalQuantity();
 
-			selectBrand.addEventListener('change', function () {
-				const brandValue = selectBrand.options[selectBrand.selectedIndex].value;
-				const brandText = selectBrand.options[selectBrand.selectedIndex].text;
-				const modelOptions = selectModel.querySelectorAll('option');
-				const selectWrap = selectBrand.closest('.form-elem__wrap');
+				insertText();
+			});
 
-				totalText.brand = brandText;
+			counterPlusBtn.addEventListener('click', function () {
+				cardObj.quantityPlus(index);
+				cardObj.calcPrice(index);
+				cardObj.calcTotalQuantity();
 
-				selectWrap.classList.add('checked');
+				insertText();
+			});
 
-				resetModelSelect(modelOptions);
-				closeOptions(modelOptions);
+			inputs.forEach(function (input) {
+				input.addEventListener('change', function () {
+					const defaultPrice = input.getAttribute('data-default-price');
 
-				modelOptions.forEach(function (modelOption) {
-					const modelGroup = modelOption.getAttribute('data-group');
-					const isMatched = modelGroup === brandValue;
+					cardObj.cardArr[index].price = defaultPrice;
+					cardObj.calcPrice(index);
+					cardObj.calcTotalQuantity();
 
-					if(isMatched) {
-						openOptions(modelOption);
-					}
+					insertText();
 				});
-
-				disableNextBtn();
-			});
-
-			selectModel.addEventListener('change', function () {
-				const modelText = selectModel.options[selectModel.selectedIndex].text;
-				const selectWrap = selectModel.closest('.form-elem__wrap')
-
-				selectWrap.classList.add('checked');
-				totalText.model = modelText;
-
-				totalText.total = `${totalText.brand} ${totalText.model}`;
-
-				refreshOrderStatus();
-				permitNextBtn();
-			});
-
-			selectMalfunction.addEventListener('change', function () {
-				const selectWrap = selectMalfunction.closest('.form-elem__wrap');
-				const malfunctionText = selectMalfunction.options[selectMalfunction.selectedIndex].text;
-
-				selectWrap.classList.add('checked');
-
-				totalText.malfunction = malfunctionText;
-
-				totalText.total = `${totalText.brand} ${totalText.model}, ${totalText.malfunction}`;
-
-				refreshOrderStatus();
-			});
-
-			btnNextStep.addEventListener('click', function () {
-				closeSteps();
-
-				if(currentStep >= 2) {
-					this.classList.add('hide');
-					console.log(this);
-
-					currentStep = 2;
-				}
-
-				const nextStep = currentStep + 1;
-				const nextStepNodesStroke = `.js-steps-${nextStep}`;
-				const nextStepNodes = document.querySelectorAll(`.js-steps-${nextStep}`);
-
-				currentStep++;
-
-				// car animation
-				form.classList.remove(`step-1`);
-				form.classList.remove(`step-2`);
-				form.classList.add(`step-${nextStep}`);
-
-				goToStep(nextStepNodes);
 			});
 		});
 	};
 
-	const headerScroll = function () {
-		const $header = $('.js-header');
-
-		$(window).on('scroll', function () {
-			if($(this).scrollTop() > 50){
-				$header.addClass('scrolled');
-			}else{
-				$header.removeClass('scrolled');
-			}
-		});
+	const onPageRdy = function () {
+		cardCalc();
 	};
 
-	// sliders
-	personCardSlider();
-	advantagesSlider();
-	popularServices();
-
-	// others
-	selectChange();
-	phoneMask();
-	checkboxLink();
-	menu();
-	avtoSelect();
-	headerScroll();
+	onPageRdy();
 })();
